a426afcaceafded83b19f6e2137c269fa297b2b4
Dailius <dailius.rascius@gmail.com>
2021-07-27 17:20:17 +0300

    Add BaseApplication.kt and libraries impl

---
+++ app/src/main/java/com/dailiusprograming/newsapp/app/BaseApplication.kt
@@ -0,0 +1,16 @@
+package com.dailiusprograming.newsapp.app
+
+import android.app.Application
+import androidx.viewbinding.BuildConfig
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class BaseApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG){
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
