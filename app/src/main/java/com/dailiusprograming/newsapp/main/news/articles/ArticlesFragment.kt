3cd8d0419124e2f6a920ec36474f5db666ae24b3
Dailius <dailius.rascius@gmail.com>
2021-07-28 15:35:19 +0300

    Extend ArticlesFragment with BaseFragment

--- app/src/main/java/com/dailiusprograming/newsapp/main/news/articles/ArticlesFragment.kt
+++ app/src/main/java/com/dailiusprograming/newsapp/main/news/articles/ArticlesFragment.kt
@@ -1,13 +1,15 @@
 package com.dailiusprograming.newsapp.main.news.articles
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.dailiusprograming.newsapp.R
+import com.dailiusprograming.newsapp.utils.fragment.BaseFragment
 
-class ArticlesFragment : Fragment() {
+class ArticlesFragment : BaseFragment() {
+    override val layoutRes get() = R.layout.fragment_articles
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
