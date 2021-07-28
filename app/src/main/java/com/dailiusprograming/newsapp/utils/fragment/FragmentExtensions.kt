be9227f95c4642ddeb5d3b13df12110420b3a5e0
Dailius <dailius.rascius@gmail.com>
2021-07-28 16:35:47 +0300

    Set newsContainer in FragmentExtensions.kt

--- app/src/main/java/com/dailiusprograming/newsapp/utils/fragment/FragmentExtensions.kt
+++ app/src/main/java/com/dailiusprograming/newsapp/utils/fragment/FragmentExtensions.kt
@@ -3,10 +3,9 @@ package com.dailiusprograming.newsapp.utils.fragment
 import androidx.fragment.app.Fragment
 import com.dailiusprograming.newsapp.R
 
-fun Fragment.openFragment(fragment: Fragment, addToBackStack: Boolean){
-    // TODO replace R.id.container with correct one
-    val transaction = childFragmentManager.beginTransaction().replace(R.id.container, fragment)
-    if (addToBackStack){
+fun Fragment.openFragment(fragment: Fragment, addToBackStack: Boolean) {
+    val transaction = childFragmentManager.beginTransaction().replace(R.id.newsContainer, fragment)
+    if (addToBackStack) {
         transaction.addToBackStack(null)
     }
     transaction.commit()
