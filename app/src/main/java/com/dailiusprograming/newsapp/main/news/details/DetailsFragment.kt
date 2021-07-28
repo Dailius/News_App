1621d99c90bb27fddbcf495166bdeb4facd12dc1
Dailius <dailius.rascius@gmail.com>
2021-07-28 15:41:32 +0300

    Extend DetailsFragment with BaseFragment

--- app/src/main/java/com/dailiusprograming/newsapp/main/news/details/DetailsFragment.kt
+++ app/src/main/java/com/dailiusprograming/newsapp/main/news/details/DetailsFragment.kt
@@ -1,13 +1,15 @@
 package com.dailiusprograming.newsapp.main.news.details
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.dailiusprograming.newsapp.R
+import com.dailiusprograming.newsapp.utils.fragment.BaseFragment
 
-class DetailsFragment : Fragment() {
+class DetailsFragment : BaseFragment() {
+    override val layoutRes get() = R.layout.fragment_details
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
