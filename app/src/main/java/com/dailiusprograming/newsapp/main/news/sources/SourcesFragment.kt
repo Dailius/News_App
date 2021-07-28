fab6946df690eebe726ecc3d0d718c61f8429c5a
Dailius <dailius.rascius@gmail.com>
2021-07-28 15:28:53 +0300

    Set up ViewBinding in SourcesFragment.kt

--- app/src/main/java/com/dailiusprograming/newsapp/main/news/sources/SourcesFragment.kt
+++ app/src/main/java/com/dailiusprograming/newsapp/main/news/sources/SourcesFragment.kt
@@ -5,19 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.dailiusprograming.newsapp.R
+import com.dailiusprograming.newsapp.databinding.FragmentSourcesBinding
 import com.dailiusprograming.newsapp.utils.fragment.BaseFragment
 
 class SourcesFragment : BaseFragment() {
     override val layoutRes get() = R.layout.fragment_sources
+    private var _binding: FragmentSourcesBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sources, container, false)
+    ): View {
+        _binding = FragmentSourcesBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     companion object {
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
     }
 }
