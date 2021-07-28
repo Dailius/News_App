7db4bb9fcead4fdd0b067f5e33662d1a128fea70
Dailius <dailius.rascius@gmail.com>
2021-07-28 15:26:29 +0300

    Extend SourcesFragment with BaseFragment

--- app/src/main/java/com/dailiusprograming/newsapp/main/news/sources/SourcesFragment.kt
+++ app/src/main/java/com/dailiusprograming/newsapp/main/news/sources/SourcesFragment.kt
@@ -1,13 +1,15 @@
 package com.dailiusprograming.newsapp.main.news.sources
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.dailiusprograming.newsapp.R
+import com.dailiusprograming.newsapp.utils.fragment.BaseFragment
 
-class SourcesFragment : Fragment() {
+class SourcesFragment : BaseFragment() {
+    override val layoutRes get() = R.layout.fragment_sources
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
