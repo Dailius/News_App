6c228bc9cb90943186a36de4a2ad88f90e993119
Dailius <dailius.rascius@gmail.com>
2021-07-28 15:44:18 +0300

    Set up ViewBinding in DetailsFragment

--- app/src/main/java/com/dailiusprograming/newsapp/main/news/details/DetailsFragment.kt
+++ app/src/main/java/com/dailiusprograming/newsapp/main/news/details/DetailsFragment.kt
@@ -5,19 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.dailiusprograming.newsapp.R
+import com.dailiusprograming.newsapp.databinding.FragmentDetailsBinding
 import com.dailiusprograming.newsapp.utils.fragment.BaseFragment
 
 class DetailsFragment : BaseFragment() {
     override val layoutRes get() = R.layout.fragment_details
+    private var _binding: FragmentDetailsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false)
+    ): View {
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     companion object {
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
     }
 }
