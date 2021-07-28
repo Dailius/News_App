1545c65a0dd1aac08e3ba6dbb4443904e8bf767e
Dailius <dailius.rascius@gmail.com>
2021-07-28 07:59:43 +0300

    Add MainPagerFragment in main/pager

---
+++ app/src/main/java/com/dailiusprograming/newsapp/main/pager/MainPagerFragment.kt
@@ -0,0 +1,9 @@
+package com.dailiusprograming.newsapp.main.pager
+
+import com.dailiusprograming.newsapp.R
+import com.dailiusprograming.newsapp.utils.fragment.BaseFragment
+
+class MainPagerFragment : BaseFragment() {
+    override val layoutRes: Int
+        get() = R.layout.fragment_main_pager
+}
