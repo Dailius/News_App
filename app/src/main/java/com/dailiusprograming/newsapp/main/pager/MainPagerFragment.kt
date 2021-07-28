b1870908fc42f41c818052f21c4d11497de77b6f
Dailius <dailius.rascius@gmail.com>
2021-07-28 12:09:16 +0300

    Set up ViewPager and BottomNav in MainPagerFragment

--- app/src/main/java/com/dailiusprograming/newsapp/main/pager/MainPagerFragment.kt
+++ app/src/main/java/com/dailiusprograming/newsapp/main/pager/MainPagerFragment.kt
@@ -1,11 +1,83 @@
 package com.dailiusprograming.newsapp.main.pager
 
-import androidx.viewpager.widget.PagerAdapter
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
 import com.dailiusprograming.newsapp.R
+import com.dailiusprograming.newsapp.databinding.FragmentMainPagerBinding
 import com.dailiusprograming.newsapp.utils.fragment.BaseFragment
 
 class MainPagerFragment : BaseFragment() {
-    override val layoutRes: Int
-        get() = R.layout.fragment_main_pager
-    private lateinit var adapter: PagerAdapter
+    override val layoutRes = R.layout.fragment_main_pager
+    private lateinit var adapter: MainPagerAdapter
+    private var _binding: FragmentMainPagerBinding? = null
+    private val binding = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        adapter = MainPagerAdapter(fragmentManager = childFragmentManager, lifecycle = lifecycle)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMainPagerBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpBottomNavigationView()
+        setUpViewPager()
+    }
+
+    private fun setUpBottomNavigationView() {
+        binding.apply {
+            bottomNavigationView.setOnItemSelectedListener { menuItem ->
+                when (menuItem.itemId) {
+                    R.id.newsMenuItem -> {
+                        mainViewPager.currentItem = MainPagerAdapter.NEWS_PAGE
+                        true
+                    }
+                    R.id.favoriteMenuItem -> {
+                        mainViewPager.currentItem = MainPagerAdapter.FAVORITE_PAGE
+                        true
+                    }
+                    R.id.aboutMenuItem -> {
+                        mainViewPager.currentItem = MainPagerAdapter.ABOUT_PAGE
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }
+    }
+
+    private fun setUpViewPager() {
+        binding.apply {
+            mainViewPager.apply {
+                adapter = this@MainPagerFragment.adapter
+                registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                    override fun onPageSelected(position: Int) {
+                        super.onPageSelected(position)
+                        bottomNavigationView.menu.getItem(position).isChecked = true
+                    }
+                })
+                offscreenPageLimit = 2
+            }
+        }
+    }
+
+    companion object {
+        fun newInstance() = MainPagerFragment()
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
 }
