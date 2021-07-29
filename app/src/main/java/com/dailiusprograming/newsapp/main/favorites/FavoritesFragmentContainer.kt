81666880abd2634008a133f5e8651090b69ff075
Dailius Raščius <dailius.rascius@telesoftas.com>
2021-07-29 23:45:02 +0300

    Display FavoritesFragment in FavoritesFragmentContainer

--- app/src/main/java/com/dailiusprograming/newsapp/main/favorites/FavoritesFragmentContainer.kt
+++ app/src/main/java/com/dailiusprograming/newsapp/main/favorites/FavoritesFragmentContainer.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import com.dailiusprograming.newsapp.R
 import com.dailiusprograming.newsapp.databinding.FragmentFavoritesContainerBinding
+import com.dailiusprograming.newsapp.main.pager.MainPagerAdapter
 import com.dailiusprograming.newsapp.utils.fragment.BaseFragment
+import com.dailiusprograming.newsapp.utils.fragment.openFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,6 +17,13 @@ class FavoritesFragmentContainer : BaseFragment() {
     private var _binding: FragmentFavoritesContainerBinding? = null
     private val binding get() = _binding!!
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (savedInstanceState == null) {
+            openFavoritesFragment()
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,6 +31,14 @@ class FavoritesFragmentContainer : BaseFragment() {
     ): View {
         _binding = FragmentFavoritesContainerBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    private fun openFavoritesFragment() {
+        openFragment(
+            FavoritesFragment.newInstance(),
+            addToBackStack = false,
+            pageConstFromMainPagerAdapter = MainPagerAdapter.FAVORITES_PAGE
+        )
     }
 
     override fun onBackPressed() = childFragmentManager.popBackStackImmediate()
