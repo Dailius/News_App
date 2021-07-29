6c8183bbc4cc2cf22b7aef8e727beffd38789ebf
Dailius Raščius <dailius.rascius@telesoftas.com>
2021-07-29 23:35:47 +0300

    Add viewBinding in FavoritesFragment

--- app/src/main/java/com/dailiusprograming/newsapp/main/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/dailiusprograming/newsapp/main/favorites/FavoritesFragment.kt
@@ -1,21 +1,31 @@
 package com.dailiusprograming.newsapp.main.favorites
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.dailiusprograming.newsapp.R
+import com.dailiusprograming.newsapp.databinding.FragmentFavoritesBinding
+import com.dailiusprograming.newsapp.utils.fragment.BaseFragment
 
-class FavoritesFragment : Fragment() {
+class FavoritesFragment : BaseFragment() {
+    override val layoutRes get() = R.layout.fragment_favorites
+    private var _binding: FragmentFavoritesBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorites, container, false)
+    ): View {
+        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     companion object {
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
     }
 }
