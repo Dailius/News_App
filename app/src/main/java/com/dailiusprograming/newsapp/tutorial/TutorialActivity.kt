76cc635ecdec85eb5698a700a9ac021d4148ec30
Dailius <dailius.rascius@gmail.com>
2021-08-04 14:53:45 +0300

    Add view binding in TutorialActivity

--- app/src/main/java/com/dailiusprograming/newsapp/tutorial/TutorialActivity.kt
+++ app/src/main/java/com/dailiusprograming/newsapp/tutorial/TutorialActivity.kt
@@ -2,11 +2,17 @@ package com.dailiusprograming.newsapp.tutorial
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.dailiusprograming.newsapp.R
+import com.dailiusprograming.newsapp.databinding.ActivityTutorialBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class TutorialActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTutorialBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tutorial)
+        binding = ActivityTutorialBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
