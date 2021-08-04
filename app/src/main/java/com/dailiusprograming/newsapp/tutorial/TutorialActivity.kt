862940105cd917c24bfa01c283ce2a85accb5b96
Dailius <dailius.rascius@gmail.com>
2021-08-04 14:37:02 +0300

    Add tutorial activity into newsapp/tutorial

---
+++ app/src/main/java/com/dailiusprograming/newsapp/tutorial/TutorialActivity.kt
@@ -0,0 +1,12 @@
+package com.dailiusprograming.newsapp.tutorial
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dailiusprograming.newsapp.R
+
+class TutorialActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutorial)
+    }
+}
