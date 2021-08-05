fd664773e62d1b9e0fc21043f353332cdee97160
Dailius <dailius.rascius@gmail.com>
2021-08-05 23:49:19 +0300

    Add TimeConverter into MainDatabase

--- app/src/main/java/com/dailiusprograming/newsapp/utils/storage/MainDatabase.kt
+++ app/src/main/java/com/dailiusprograming/newsapp/utils/storage/MainDatabase.kt
@@ -2,12 +2,14 @@ package com.dailiusprograming.newsapp.utils.storage
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.dailiusprograming.newsapp.utils.storage.MainDatabase.Companion.DB_VERSION
 
 @Database(
     entities = [],
     version = DB_VERSION
 )
+@TypeConverters(TimeConverter::class)
 abstract class MainDatabase : RoomDatabase() {
     companion object{
         const val DB_VERSION = 1
