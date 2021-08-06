1ec7140766177a398f61c7d91f50d93fe4e34e3d
Dailius <dailius.rascius@gmail.com>
2021-08-06 10:35:24 +0300

    Add getSourceDao() and entities in MainDatabase

--- app/src/main/java/com/dailiusprograming/newsapp/utils/storage/MainDatabase.kt
+++ app/src/main/java/com/dailiusprograming/newsapp/utils/storage/MainDatabase.kt
@@ -3,14 +3,18 @@ package com.dailiusprograming.newsapp.utils.storage
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.dailiusprograming.newsapp.main.news.sources.data.local.SourceDao
+import com.dailiusprograming.newsapp.main.news.sources.data.model.SourceEntity
 import com.dailiusprograming.newsapp.utils.storage.MainDatabase.Companion.DB_VERSION
 
 @Database(
-    entities = [],
+    entities = [SourceEntity::class],
     version = DB_VERSION
 )
 @TypeConverters(TimeConverter::class)
 abstract class MainDatabase : RoomDatabase() {
+    abstract fun getSourceDao(): SourceDao
+
     companion object {
         const val DB_VERSION = 1
     }
