8ded2edc802fa02d8b1393548808479096d46598
Dailius <dailius.rascius@gmail.com>
2021-08-10 10:54:48 +0300

    Set MutableLiveData variables in ArticlesViewModel

--- app/src/main/java/com/dailiusprograming/newsapp/main/news/articles/ArticlesViewModel.kt
+++ app/src/main/java/com/dailiusprograming/newsapp/main/news/articles/ArticlesViewModel.kt
@@ -1,6 +1,10 @@
 package com.dailiusprograming.newsapp.main.news.articles
 
+import androidx.lifecycle.MutableLiveData
+import com.dailiusprograming.newsapp.main.news.articles.data.model.ArticleDomain
+import com.dailiusprograming.newsapp.main.news.articles.data.model.ArticleError
 import com.dailiusprograming.newsapp.main.news.articles.repository.FetchArticlesUseCase
+import com.dailiusprograming.newsapp.main.news.articles.utils.ArticlesFilterType
 import com.dailiusprograming.newsapp.utils.fragment.BaseViewModel
 import com.dailiusprograming.newsapp.utils.scheduler.Main
 import io.reactivex.rxjava3.core.Scheduler
@@ -10,5 +14,15 @@ class ArticlesViewModel @Inject constructor(
     private val articlesUseCase: FetchArticlesUseCase,
     @Main private val scheduler: Scheduler
 ) : BaseViewModel() {
+    private var _articleList = MutableLiveData<List<ArticleDomain>>()
+    val articleList = _articleList
 
+    private val _isLoadingLiveData = MutableLiveData(false)
+    val isLoadingLiveData = _isLoadingLiveData
+
+    private val _errorMessage = MutableLiveData<ArticleError>()
+    val errorMessage = _errorMessage
+
+    private var articlesFilterType: ArticlesFilterType = ArticlesFilterType.TODAY
+    private lateinit var sourceId: String
 }
