796bfc44583c3c4877e250bf0310cedc7d726161
Dailius <dailius.rascius@gmail.com>
2021-08-10 10:52:07 +0300

    Hilt injects into constructor of ArticlesViewModel

--- app/src/main/java/com/dailiusprograming/newsapp/main/news/articles/ArticlesViewModel.kt
+++ app/src/main/java/com/dailiusprograming/newsapp/main/news/articles/ArticlesViewModel.kt
@@ -1,7 +1,14 @@
 package com.dailiusprograming.newsapp.main.news.articles
 
+import com.dailiusprograming.newsapp.main.news.articles.repository.FetchArticlesUseCase
 import com.dailiusprograming.newsapp.utils.fragment.BaseViewModel
+import com.dailiusprograming.newsapp.utils.scheduler.Main
+import io.reactivex.rxjava3.core.Scheduler
+import javax.inject.Inject
 
-class ArticlesViewModel: BaseViewModel() {
+class ArticlesViewModel @Inject constructor(
+    private val articlesUseCase: FetchArticlesUseCase,
+    @Main private val scheduler: Scheduler
+) : BaseViewModel() {
 
 }
