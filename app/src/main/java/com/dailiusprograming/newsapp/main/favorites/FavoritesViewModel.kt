55fb1af27a0e800be5819132906dfbfce1596f15
Dailius <dailius.rascius@gmail.com>
2021-08-15 15:24:36 +0300

    Add Hilt Injects in FavoritesViewModel.kt

--- app/src/main/java/com/dailiusprograming/newsapp/main/favorites/FavoritesViewModel.kt
+++ app/src/main/java/com/dailiusprograming/newsapp/main/favorites/FavoritesViewModel.kt
@@ -1,7 +1,16 @@
 package com.dailiusprograming.newsapp.main.favorites
 
+import com.dailiusprograming.newsapp.main.favorites.repository.FetchFavoritesUseCase
 import com.dailiusprograming.newsapp.utils.fragment.BaseViewModel
+import com.dailiusprograming.newsapp.utils.scheduler.Main
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.core.Scheduler
+import javax.inject.Inject
 
-class FavoritesViewModel : BaseViewModel() {
+@HiltViewModel
+class FavoritesViewModel @Inject constructor(
+    private val favoritesUseCase: FetchFavoritesUseCase,
+    @Main private val scheduler: Scheduler
+) : BaseViewModel(){
 
 }
