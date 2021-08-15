8f503849427bfd4af2fa68838e8ef55590af585c
Dailius <dailius.rascius@gmail.com>
2021-08-15 14:38:39 +0300

    Set RoomDao for favorites in utils/storage

--- app/src/main/java/com/dailiusprograming/newsapp/utils/storage/MainDatabase.kt
+++ app/src/main/java/com/dailiusprograming/newsapp/utils/storage/MainDatabase.kt
@@ -3,6 +3,7 @@ package com.dailiusprograming.newsapp.utils.storage
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.dailiusprograming.newsapp.main.favorites.data.local.FavoritesDao
 import com.dailiusprograming.newsapp.main.news.articles.data.local.ArticleDao
 import com.dailiusprograming.newsapp.main.news.articles.data.model.ArticleEntity
 import com.dailiusprograming.newsapp.main.news.details.data.local.DetailsDao
@@ -22,6 +23,7 @@ abstract class MainDatabase : RoomDatabase() {
     abstract fun getSourceDao(): SourceDao
     abstract fun getArticleDao(): ArticleDao
     abstract fun getDetailsDao(): DetailsDao
+    abstract fun getFavoritesDao(): FavoritesDao
 
     companion object {
         const val DB_VERSION = 1
