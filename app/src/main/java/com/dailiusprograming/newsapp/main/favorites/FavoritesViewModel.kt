7f0620ac352529702dccf2a861fd423d5105b70c
Dailius <dailius.rascius@gmail.com>
2021-08-15 15:29:32 +0300

    Add live data variables in FavoritesViewModel

--- app/src/main/java/com/dailiusprograming/newsapp/main/favorites/FavoritesViewModel.kt
+++ app/src/main/java/com/dailiusprograming/newsapp/main/favorites/FavoritesViewModel.kt
@@ -1,7 +1,11 @@
 package com.dailiusprograming.newsapp.main.favorites
 
+import androidx.lifecycle.MutableLiveData
 import com.dailiusprograming.newsapp.main.favorites.repository.FetchFavoritesUseCase
+import com.dailiusprograming.newsapp.main.news.articles.data.model.ArticleDomain
+import com.dailiusprograming.newsapp.main.news.articles.data.model.ArticleError
 import com.dailiusprograming.newsapp.utils.fragment.BaseViewModel
+import com.dailiusprograming.newsapp.utils.fragment.LiveEvent
 import com.dailiusprograming.newsapp.utils.scheduler.Main
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.rxjava3.core.Scheduler
@@ -11,6 +15,12 @@ import javax.inject.Inject
 class FavoritesViewModel @Inject constructor(
     private val favoritesUseCase: FetchFavoritesUseCase,
     @Main private val scheduler: Scheduler
-) : BaseViewModel(){
+) : BaseViewModel() {
+
+    private var _favoritesList = MutableLiveData<List<ArticleDomain>>()
+    val favoritesList = _favoritesList
+
+    private var _errorMessage = LiveEvent<ArticleError>()
+    val errorMessage = _errorMessage
 
 }
